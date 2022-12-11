5141c6d7829d8d67e2c1504f9c86c60f3116cf8d
Wendy Karstan <49969770+TerraEdition@users.noreply.github.com>
2022-12-11 16:07:24 +0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -47,8 +47,10 @@ function showModal() {
   a.addEventListener("click", function () {
     if (a.dataset.index == "aws1") {
       modalBody.innerHTML = `<img src="assets/img/sertifikat/aws.jpeg" alt="AWS 1">`;
+      modalFooter.innerHTML = "";
     } else if (a.dataset.index == "aws2") {
       modalBody.innerHTML = `<img src="assets/img/sertifikat/aws-2.jpeg" alt="AWS 2">`;
+      modalFooter.innerHTML = "";
     } else if (a.dataset.index == "apo-siak") {
       modalBody.innerHTML = `Aplikasi Online RSUD Tengku Rafi'an ini merupakan aplikasi yang diperuntukkan bagi pasien Rumah Sakit Umum Daerah Tengku Rafi'an Kabupaten Siak yang ingin melakukan pendaftaran secara online. Selain dipergunakan untuk pendaftaran online, aplikasi ini juga berisi informasi-informasi mengenai jadwal dokter, jadwal dokter cuti, informasi unggulan lainnya`;
       modalFooter.innerHTML = `<a href="https://play.google.com/store/apps/details?id=terraedition.rsud.siak" target="_blank"
